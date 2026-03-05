b071bb6a8f9af7c013cbc3eea1ae21f9ee6f19e5
Sunilkumar Bharathinagar <sunilbr.sunilbr@gmail.com>
2026-03-04 19:29:42 -0600

    added a method in FeaturePage.java

--- src/main/java/Feature/FeaturePage.java
+++ src/main/java/Feature/FeaturePage.java
@@ -2,6 +2,10 @@ package Feature;
 
 public class FeaturePage {
     public void f1(){
-        System.out.println("f1 feature - gitlearning");
+        System.out.println("f1 feature - gitlearning - tag");
+    }
+
+    public void f2(){
+        System.out.println("f2 feature - gitlearning -tag");
     }
 }
